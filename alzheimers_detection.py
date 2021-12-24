3d7b2c72c89ff618188785b91e973db6400373b2
JRose31 <jroseborough31@gmail.com>
2021-12-24 15:00:00 -0800

    Revised image reading function and created training and testing sets

--- alzheimers_detection.py
+++ alzheimers_detection.py
@@ -6,48 +6,65 @@ Created on Thu Dec 23 18:30:44 2021
 """
 import numpy as np
 import cv2
+import glob
 import matplotlib.pyplot as plt
+import time
 
-def cv2_read_path(set_type, stages, img_counts):
+def cv2_read_path_train(set_type, stages):
     ''' Processes images given the file path and naming conventions'''
     
+    # list to hold image data - converted to numpy array on return
     image_array = []
     
+    # list to hold labels - converted to numpy array on return
     label_array = []
-    # Dictionary to map folder name (dementia stage) with image naming convention
-    name_dict = {'NonDemented' : ['nonDem', 0],
-                 'VeryMildDemented' : ['verymildDem', 1],
-                 'MildDemented' : ['mildDem', 2],
-                 'ModerateDemented' : ['moderateDem', 3]}
     
-    for stage, img_count in zip(stages, img_counts):
-        # Using image count to get specific number of images in specified folder
-        for idx in range(img_count+1):
-            # Match folder name (stage) and image naming convention
-            for val in name_dict.keys():
-                if stage == val:
-                    # Construct file path
-                    img_name = name_dict[val][0]
-                    img_path = f'Alzheimers_Dataset/{set_type}/{stage}/{img_name+str(idx)}.jpg'
-                    # Read and show image
-                    IMG_SIZE = 100
-                    img = cv2.imread(img_path, 0)
-                    new_array = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-                    image_array.append(new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)/255)
-                    # Add label
-                    label_array.append(name_dict[val][1])
+    # Label encoding dictionary
+    name_dict = {'NonDemented' : 0,
+                 'VeryMildDemented' : 1,
+                 'MildDemented' : 2,
+                 'ModerateDemented' : 3}
+    
+    # Iterate through stages
+    for stage in stages:
+        # Iterate through every image in specified directory
+        for filename in glob.glob(f'Alzheimers_Dataset/{set_type}/{stage}/*.jpg'):
+            # Read image
+            IMG_SIZE = 100
+            img = cv2.imread(filename, 0)
+            new_array = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+            # Add image to list, SCALED
+            image_array.append(new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)/255)
+            # Add label
+            label_array.append(name_dict[stage])
 
     return np.array(image_array)[:,0,:,:,:], np.array(label_array)             
 
+# Name of dir for each stage
 all_stages = ['NonDemented', 'VeryMildDemented', 'MildDemented', 'ModerateDemented']
-all_counts = [2559, 1791, 716, 51]
 
-trainX, trainy = cv2_read_path(set_type='train', stages=all_stages, img_counts=all_counts)
+# Create trainng sets
+X_train, y_train = cv2_read_path_train(set_type='train', stages=all_stages)
 
+# Create testing sets
+X_test, y_test = cv2_read_path_train(set_type='test', stages=all_stages)
+
+# Function to show image from dataset
 def show_sample(data, index):
      plt.imshow(data[index], cmap=plt.cm.binary)
      plt.show()
-     
+
+# View some TRAINING MRI scans
+print('Training Set:')
 for n in range(1,21):
-    show_sample(trainX, -n)
-    print(f'Label: {trainy[-n]}')
+    show_sample(X_train, -n)
+    print(f'Label - {y_train[-n]}')
+    
+time.sleep(3)
+
+# View some TESTING MRI scans
+print('Testing Set:')
+for n in range(1,21):
+    show_sample(X_test, -n)
+    print(f'Label - {y_test[-n]}')
+    
