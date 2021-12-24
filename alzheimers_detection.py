f7f1438b2b7e9fbe165b74307efadd2d3607184b
JRose31 <jroseborough31@gmail.com>
2021-12-24 08:41:15 -0800

    Created array of labels for dementia stages

--- alzheimers_detection.py
+++ alzheimers_detection.py
@@ -13,11 +13,12 @@ def cv2_read_path(set_type, stage, img_count):
     
     image_array = []
     
+    label_array = []
     # Dictionary to map folder name (dementia stage) with image naming convention
-    name_dict = {'NonDemented' : 'nonDem',
-                 'VeryMildDemented' : 'verymildDem',
-                 'MildDemented' : 'mildDem',
-                 'ModerateDemented' : 'moderateDem'}
+    name_dict = {'NonDemented' : ['nonDem', 0],
+                 'VeryMildDemented' : ['verymildDem', 1],
+                 'MildDemented' : ['mildDem', 2],
+                 'ModerateDemented' : ['moderateDem', 3]}
     
     # Using image count to get specific number of images in specified folder
     for idx in range(img_count+1):
@@ -25,25 +26,28 @@ def cv2_read_path(set_type, stage, img_count):
         for val in name_dict.keys():
             if stage == val:
                 # Construct file path
-                img_name = name_dict[val]
+                img_name = name_dict[val][0]
                 img_path = f'Alzheimers_Dataset/{set_type}/{stage}/{img_name+str(idx)}.jpg'
                 # Read and show image
                 IMG_SIZE = 100
                 img = cv2.imread(img_path, 0)
                 new_array = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
                 image_array.append(new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)/255)
+                # Add label
+                label_array.append(name_dict[val][1])
 
-    return np.array(image_array)[:,0,:,:,:]             
+    return np.array(image_array)[:,0,:,:,:], label_array             
 
 
-nonDemdata = cv2_read_path(set_type='train',stage='NonDemented', img_count=2559)
-verymildDemdata = cv2_read_path(set_type='train',stage='VeryMildDemented', img_count=1791)
-mildDemdata = cv2_read_path(set_type='train',stage='MildDemented', img_count=716)
-moderateDemdata = cv2_read_path(set_type='train',stage='ModerateDemented', img_count=51)
+nonDemdataX, nonDemdatay = cv2_read_path(set_type='train',stage='NonDemented', img_count=2559)
+verymildDemdataX, verymildDemdatay = cv2_read_path(set_type='train',stage='VeryMildDemented', img_count=1791)
+mildDemdataX, mildDemdatay = cv2_read_path(set_type='train',stage='MildDemented', img_count=716)
+moderateDemdataX, moderateDemdatay = cv2_read_path(set_type='train',stage='ModerateDemented', img_count=51)
 
 def show_sample(data, index):
     plt.imshow(data[index], cmap=plt.cm.binary)
     plt.show()
     
-for n in range(500):
-    show_sample(nonDemdata, n)
+for n in range(20):
+    show_sample(moderateDemdataX, n)
+    print(f'Label: {moderateDemdatay[n]}')
